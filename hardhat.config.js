322b3caa7d6f04af7a8d599c1d1f4ae48ed5c79d
Tanay Arun Ankulwar <ankulwartanay@gmail.com>
2024-03-12 07:40:56 +0530

    changes

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+};
